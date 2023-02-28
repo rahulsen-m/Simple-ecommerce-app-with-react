5abc41c825764b38c0d7a6ed5c12e46a002d8b39
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-02-28 12:37:35 +0530

    Example of nested routeings / practice

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import { Route, Routes } from "react-router-dom";
+import { Outlet, Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home.componet";
 
+const Nevigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>I am from Nevigation bar component..</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+
 const Shop = () => {
-  return <h1>I am from shop component!!</h1>;
+  return (
+    <div>
+      <h2>I am from shop component!!</h2>
+      <Outlet />
+    </div>
+  );
+};
+
+const Billing = () => {
+  return <h3>I am bililing compnent</h3>;
 };
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />}>
-        <Route path="/shop" element={<Shop />} />
+      <Route path="/" element={<Nevigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />}>
+          <Route path="billing" element={<Billing />} />
+        </Route>
       </Route>
     </Routes>
   );
