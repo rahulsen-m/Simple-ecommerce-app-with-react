5f848290534032e82f44d849fd6f799124be7112
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-02-28 13:52:08 +0530

    Added the basic nevigation section html layout

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
 
+import "./navigation.styles.scss";
+
 const Nevigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          Logo
+          LOGO
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
