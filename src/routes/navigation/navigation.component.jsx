d4db6d986d89b494615e9f58340e08d443465f61
Rahul Sen <rahul.sen@mindfiresolutions.com>
2023-02-28 13:44:25 +0530

    Used link for adding navigation to different pages

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,25 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const Nevigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          Logo
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+          <Link className="nav-link" to="/sign-in">
+            SIGN IN
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Nevigation;
